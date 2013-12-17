10aaca6ae171c6731731114cdcfaac8492b59e8b
Sam Strasser <sam.strasser@gmail.com>
2013-12-16 18:43:56 -0800

    Add info on students and mentors to reflection data

--- one_off/reflections.sql
+++ one_off/reflections.sql
@@ -1,4 +1,21 @@
-SELECT * FROM reflection_log_entries as entries
+SELECT 
+  students.summit_id, 
+  students.first_name as student_first, 
+  students.last_name as student_last,
+  students.grade_level,
+  sites.name as school,
+  mentors.first_name as mentor_first,
+  mentors.last_name as mentor_last,
+  entries.contents as reflection_contents,
+  prompts.message as prompt,
+  entries.entry_type,
+  entries.due_on,
+  entries.completed,
+  entries.completed_at,
+  entries.created_at
+FROM reflection_log_entries as entries
 JOIN users as students ON entries.student_id = students.id
 JOIN reflection_log_prompts as prompts ON prompts.id = entries.reflection_log_prompt_id
+JOIN sites ON students.site_id = sites.id
+JOIN users as mentors ON students.mentor_id = mentors.id
 ;
