02c18df829307b80c515a0e550ad4436b4e410da
Sam Strasser <sam.strasser@gmail.com>
2013-12-27 12:14:31 -0800

    smaller of the two

--- one_off/overdue_projects.sql
+++ one_off/overdue_projects.sql
@@ -5,7 +5,7 @@ pa.submitted_on,
 pa.submitted_on is null or pa.submitted_on > pa.due_on as overdue,
 pa.submitted_on - pa.due_on as overdue_sub,
 cast(now() as date) - pa.due_on as overdue_unsub,
-pa.submitted_on - pa.due_on > cast(now() as date) - pa.due_on
+least(pa.submitted_on - pa.due_on, (cast(now() as date) - pa.due_on)) as smaller_of_the_two
 from project_assignments as pa
 order by id
 ;
