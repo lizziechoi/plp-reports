658878b385dfacfaa764a5338770fc4ee0e6f682
Sam Strasser <sam.strasser@gmail.com>
2013-12-27 11:27:24 -0800

    Integer math

--- one_off/overdue_projects.sql
+++ one_off/overdue_projects.sql
@@ -4,5 +4,5 @@ pa.due_on,
 pa.submitted_on,
 pa.submitted_on is null or pa.submitted_on>pa.due_on as overdue,
 pa.submitted_on - pa.due_on as days_overdue_for_submitted,
-now( ) - pa.due_on as days_overdue_for_unsubmitted
+cast(now() as date) - pa.due_on as days_overdue_for_unsubmitted
 from project_assignments as pa;
