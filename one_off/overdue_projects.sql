4d7b93d8b91d6bb0d1e1ee251d5b231b38ac7dc6
lizziechoi <lchoi@summitps.org>
2013-12-27 14:55:14 -0800

    Update overdue_projects.sql
    HW ...
    
    Q1: What is the scenario where my 'overdue' column is incorrect?
    A1: It was marking projects overdue when they were not due and not yet submitted - oops. 
    
    I attempted to fix that, but all I could currently think of (listed above) gives me neither a T or a F in the 'overdue' column for projects that are not due yet nor submitted yet. So that doesn't work.
    
    Q2: Can you add project name, mentor name, student name, assigner name
    A2:  Attempted (see above)

--- one_off/overdue_projects.sql
+++ one_off/overdue_projects.sql
@@ -1,11 +1,19 @@
 select
-pa.id,
+students.summit_id as student_id,
+students.last_name as student_last,
+students.first_name as student_first,
+mentors.last_name as mentor_last,
+mentors.first_name as mentor_first,
+assigners.last_name as assigner_last,
+projects.name as project_name,
+pa.id as pa_id,
 pa.due_on,
 pa.submitted_on,
-pa.submitted_on is null or pa.submitted_on > pa.due_on as overdue,
-pa.submitted_on - pa.due_on as overdue_sub,
-cast(now() as date) - pa.due_on as overdue_unsub,
+pa.submitted_on > pa.due_on or (pa.submitted_on is null and pa.due_on < cast(now() as date)) as overdue,
 least(pa.submitted_on - pa.due_on, (cast(now() as date) - pa.due_on)) as smaller_of_the_two
 from project_assignments as pa
-order by id
-;
+join users as students on students.id=pa.student_id
+join users as mentors on mentors.id = students.mentor_id
+join users as assigners on assigners.id = pa.assigned_by_id
+join projects on projects.id = pa.project_id
+order by pa.due_on desc;
