72d8237236d296bac88c19fd25c76c83395f19e5
Sam Strasser <sam.strasser@gmail.com>
2013-12-27 12:04:17 -0800

    With comparison

--- one_off/overdue_projects.sql
+++ one_off/overdue_projects.sql
@@ -2,10 +2,10 @@ select
 pa.id,
 pa.due_on,
 pa.submitted_on,
--- pa.submitted_on is null or pa.submitted_on>pa.due_on as overdue,
-(pa.status = 'assigned' and pa.due_on < now()) as overdue,
+pa.submitted_on is null or pa.submitted_on > pa.due_on as overdue,
 pa.submitted_on - pa.due_on as overdue_sub,
-cast(now() as date) - pa.due_on as overdue_unsub
+cast(now() as date) - pa.due_on as overdue_unsub,
+pa.submitted_on - pa.due_on > cast(now() as date) - pa.due_on
 from project_assignments as pa
 order by id
 ;
