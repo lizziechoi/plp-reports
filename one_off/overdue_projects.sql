2083066100c7dc064b6f18161559d8845ca08077
Sam Strasser <sam.strasser@gmail.com>
2014-01-05 17:21:44 -0800

    LC update 1 https://github.com/samstrasser/plp-reports/pull/6#issuecomment-31313391

--- one_off/overdue_projects.sql
+++ one_off/overdue_projects.sql
@@ -1,12 +1,19 @@
 select
-  pa.id,
-  pa.status,
-  pa.submitted_on,
-  pa.due_on,
-  pa.submitted_on is null or pa.submitted_on > pa.due_on as overdue
-  -- (pa.status = 'assigned' and pa.due_on < now()) or (pa.submitted_on > pa.due_on) as overdue
-  pa.submitted_on > pa.due_on or (pa.submitted_on is null and pa.due_on < cast(now() as date)) as overdue,
-  
+students.summit_id as student_id,
+students.last_name as student_last,
+-- students.first_name as student_first,
+-- mentors.last_name as mentor_last,
+-- mentors.first_name as mentor_first,
+assigners.last_name as assigner_last,
+projects.name as project_name,
+pa.due_on,
+pa.submitted_on,
+least(pa.submitted_on - pa.due_on, (cast(now() as date) - pa.due_on)) > 0 as overdue,
+least(pa.submitted_on - pa.due_on, (cast(now() as date) - pa.due_on)) as days_overdue 
 from project_assignments as pa
-order by overdue desc
+join users as students on students.id=pa.student_id
+join users as mentors on mentors.id = students.mentor_id
+join users as assigners on assigners.id = pa.assigned_by_id
+join projects on projects.id = pa.project_id
+order by pa.due_on desc
 ;
