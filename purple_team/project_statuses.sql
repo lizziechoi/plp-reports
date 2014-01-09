4a14c24a3ab5867ddd2d014b8d96e8d5a3e15686
Sam Strasser <sam.strasser@gmail.com>
2014-01-08 20:39:40 -0800

    As int

--- purple_team/project_statuses.sql
+++ purple_team/project_statuses.sql
@@ -5,6 +5,6 @@ select
   raw_score, 
   submitted_on, 
   scored_on, 
-  raw_score is null and status in ('rating','performed','scored','inRevision') as turned_in_but_not_scored
+  (raw_score is null and status in ('rating','performed','scored','inRevision'))::int as turned_in_but_not_scored
 from project_assignments as pas
 ;
