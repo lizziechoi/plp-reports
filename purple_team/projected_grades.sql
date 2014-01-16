1d43d48ae345f1c29292a4310250592f44f092a9
Sam Strasser <sam.strasser@gmail.com>
2014-01-16 10:23:53 -0800

    Update the right file one time

--- purple_team/projected_grades.sql
+++ purple_team/projected_grades.sql
@@ -2,11 +2,13 @@ select
   students.summit_id as Student_ID,
   students.first_name as Student_First,
   students.last_name as Student_Last,
+  sites.name as School,
   students.grade_level as Grade_Level,
   courses.name as Course,
   subjects.name as Subject,
   mentors.first_name as Mentor_First,
   mentors.last_name as Mentor_Last,
+
   pgs.projected_on as As_Of,
   pgs.project_pcnt as Projected_Overall_Score,
   pgs.power_on_track as On_Track_for_Power,
@@ -23,10 +25,13 @@ select
   pgs.num_projects_ungraded,
   pgs.num_projects_in_revision,
   pgs.power_pcnt
+
 from projected_grades as pgs
-join course_assignments as cas on cas.id = pgs.course_assignment_id
-join courses on courses.id = cas.course_id
-join subjects on subjects.id = courses.subject_id
-join users as students on students.id = cas.student_id
-join users as mentors on mentors.id = students.mentor_id
+left outer join course_assignments as cas on cas.id = pgs.course_assignment_id
+left outer join courses on courses.id = cas.course_id
+left outer join subjects on subjects.id = courses.subject_id
+left outer join users as students on students.id = cas.student_id
+left outer join users as mentors on mentors.id = students.mentor_id
+left outer join sites on students.site_id = sites.id
+order by sites.name
 ;
