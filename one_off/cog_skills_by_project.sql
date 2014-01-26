4c0dd095437ed7aa1c8a2b1b652c1f7a580535b3
Sam Strasser <sam.strasser@gmail.com>
2014-01-26 10:35:35 -0800

    Cog skills by project course and subject

---
+++ one_off/cog_skills_by_project.sql
@@ -0,0 +1,16 @@
+SELECT 
+  subject.name as "Subject Name",
+  courses.name as "Course Name",
+  projects.name as "Project Name",
+  csds.name as "Cog Skill Domain",
+  projects.weight as "Weight of project in course",
+  pcsds.weight as "Weight of cog skill in project",
+  projects.seventy_pcnt_score as "Cog skill score to get a 70%"
+FROM courses
+INNER JOIN subjects on courses.subject_id = courses.id
+INNER JOIN project_courses as pcs on pcs.course_id = courses.id
+INNER JOIN projects on pcs.project_id = projects.id
+LEFT OUTER JOIN project_cog_skill_domains as pcsds on pcsds.project_id = projects.id
+INNER JOIN cog_skill_domains as csds on pcsds.cog_skill_domain_id = csds.id
+order by courses.sequence
+;
