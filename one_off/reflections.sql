003eb25bf3f52e5e824d597192a90c596713827c
Sam Strasser <sam.strasser@gmail.com>
2014-02-12 10:19:04 -0800

    Add teachers

--- one_off/reflections.sql
+++ one_off/reflections.sql
@@ -1,9 +1,9 @@
 SELECT 
-  -- count(*)
-  students.summit_id, 
-  students.first_name as student_first, 
-  students.last_name as student_last,
-  students.grade_level,
+  reflectors.summit_id, 
+  reflectors.first_name as user_first, 
+  reflectors.last_name as user_last,
+  reflectors.grade_level,
+  reflectors.type as user_type,
   sites.name as school,
   mentors.first_name as mentor_first,
   mentors.last_name as mentor_last,
@@ -15,8 +15,27 @@ SELECT
   entries.completed_at,
   entries.created_at
 FROM reflection_log_entries as entries
-LEFT OUTER JOIN users as students ON entries.student_id = students.id
+LEFT OUTER JOIN users as reflectors ON entries.user_id = reflectors.id
 LEFT OUTER JOIN reflection_log_prompts as prompts ON prompts.id = entries.reflection_log_prompt_id
-LEFT OUTER JOIN sites ON students.site_id = sites.id
-LEFT OUTER JOIN users as mentors ON students.mentor_id = mentors.id
+LEFT OUTER JOIN sites ON reflectors.site_id = sites.id
+LEFT OUTER JOIN users as mentors ON reflectors.mentor_id = mentors.id
+;
+
+
+SELECT 
+  reflectors.first_name as user_first, 
+  reflectors.last_name as user_last,
+  sites.name as school,
+  entries.contents as reflection_contents,
+  prompts.message as prompt,
+  entries.entry_type,
+  entries.due_on,
+  entries.completed,
+  entries.completed_at,
+  entries.created_at
+FROM reflection_log_entries as entries
+LEFT OUTER JOIN users as reflectors ON entries.user_id = reflectors.id
+LEFT OUTER JOIN reflection_log_prompts as prompts ON prompts.id = entries.reflection_log_prompt_id
+LEFT OUTER JOIN sites ON reflectors.default_site_id = sites.id
+where reflectors.type = 'Teacher'
 ;
