2f5adfaf2c53e7cebfdb4041f5c26d40bd38d19e
Sam Strasser <sam.strasser@gmail.com>
2014-02-20 10:04:01 -0800

    More cols

--- info/assessment_attempts.sql
+++ info/assessment_attempts.sql
@@ -1,16 +1,17 @@
 SELECT 
-  courses.name,
-  subjects.name,
-  know_dos.name,  
-  assessment_takes.taken_at, 
-  course_know_dos.power, 
-  assessment_takes.num_correct, 
-  assessment_takes.num_possible,
-  know_dos.pcnt_to_pass,
-  users.last_leave_on > now() as "Is Currently Enrolled?",
-  users.summit_id,
-  users.first_name,
-  users.last_name
+  count(*)
+  -- courses.name,
+  -- subjects.name,
+  -- know_dos.name,  
+  -- assessment_takes.taken_at, 
+  -- course_know_dos.power, 
+  -- assessment_takes.num_correct, 
+  -- assessment_takes.num_possible,
+  -- know_dos.pcnt_to_pass,
+  -- users.last_leave_on > now() as "Is Currently Enrolled?",
+  -- users.summit_id,
+  -- users.first_name,
+  -- users.last_name
 FROM 
   course_assignments, 
   assessment_takes, 
@@ -20,13 +21,47 @@ FROM
   users,
   subjects
 WHERE 
-  course_assignments.course_id = course_know_dos.course_id AND
-  course_assignments.course_id = courses.id AND
-  assessment_takes.student_id = course_assignments.student_id AND
-  assessment_takes.know_do_id = course_know_dos.know_do_id AND
-  course_know_dos.know_do_id=know_dos.id AND
-  assessment_takes.student_id = users.id and 
-  courses.subject_id = subjects.id and 
-  users.summit_id > 9999 and
-  users.type = 'Student'
+assessment_takes.student_id = users.id and 
+assessment_takes.student_id = course_assignments.student_id AND
+course_assignments.course_id = course_know_dos.course_id AND
+assessment_takes.know_do_id = course_know_dos.know_do_id AND
+course_assignments.course_id = courses.id AND
+course_know_dos.know_do_id=know_dos.id AND
+courses.subject_id = subjects.id and 
 ;
+
+-- Missing from Bruce's original: 
+-- filter out focus areas for courses the student is not enrolled in
+select
+  courses.name as "course name",
+  subjects.name as "subject name",
+  know_dos.name as "focus area name",
+  assessment_takes.taken_at,
+  ckds.power,
+  assessment_takes.num_correct,
+  assessment_takes.num_possible,
+  (ats.num_correct / cast(ats.num_possible as float)) as pcnt_correct,
+  (ats.num_correct / cast(ats.num_possible as float)) >= kds.pcnt_to_pass as passed
+  know_dos.pcnt_to_pass,
+  users.last_leave_on > now() as "is currently enrolled?",
+  users.summit_id,
+  users.first_name,
+  users.last_name,
+  sites.name as school
+from assessment_takes as ats
+join know_dos as kds on ats.know_do_id = kds.id
+join course_know_dos as ckds on ckds.know_do_id = kds.id
+join courses on ckds.course_id = courses.id
+join subjects on courses.subject_id = subjects.id
+join users as students on students.id = ats.student_id
+left outer join sites on students.site_id = sites.id
+where students.summit_id > 9999
+and students.type = 'Student'
+;
+
+
+
+
+-- By kid, then assessment, then by date,
+-- Add pcnt col
+-- Passed
--- one_off/focus_area_by_course.sql
+++ one_off/focus_area_by_course.sql
@@ -8,3 +8,18 @@ join course_know_dos as ckds on ckds.know_do_id = kds.id
 join courses  on ckds.course_id = courses.id
 join subjects on courses.subject_id = subjects.id
 ;
+
+
+-- Focus areas in multiple courses
+select 
+  kds.id, 
+  kds.name, 
+  count(courses.id) as cnt, 
+  array_to_string(array_agg(courses.name), '; ') as courses
+from know_dos as kds
+join course_know_dos as ckds on ckds.know_do_id = kds.id
+join courses on courses.id = ckds.course_id
+group by kds.id
+having count(courses.id) > 1
+order by cnt desc, min(courses.id)
+;
