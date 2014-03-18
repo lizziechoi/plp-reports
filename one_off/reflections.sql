fa0dcc4cc87148a4e7c306677d52208ef9989883
Sam Strasser <sam.strasser@gmail.com>
2014-03-17 19:55:27 -0700

    Add tags to reflections reports

--- one_off/reflections.sql
+++ one_off/reflections.sql
@@ -9,6 +9,7 @@ SELECT
   mentors.last_name as mentor_last,
   entries.contents as reflection_contents,
   prompts.message as prompt,
+  tags.tag_string as tag_list,  
   entries.entry_type,
   entries.due_on,
   entries.completed,
@@ -19,6 +20,15 @@ LEFT OUTER JOIN users as reflectors ON entries.user_id = reflectors.id
 LEFT OUTER JOIN reflection_log_prompts as prompts ON prompts.id = entries.reflection_log_prompt_id
 LEFT OUTER JOIN sites ON reflectors.site_id = sites.id
 LEFT OUTER JOIN users as mentors ON reflectors.mentor_id = mentors.id
+LEFT OUTER JOIN (
+  select 
+    entries.id as entry_id,
+    array_to_string(array_agg(tags.name),'; ') as tag_string
+  from reflection_log_entries as entries
+  left outer join reflection_log_entry_reflection_log_tags as entry_tags on entry_tags.reflection_log_entry_id = entries.id
+  left outer join reflection_log_tags as tags on entry_tags.reflection_log_tag_id = tags.id
+  group by entries.id
+) as tags on tags.entry_id = entries.id
 ;
 
 
