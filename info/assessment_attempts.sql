8e51e0727d0ff10316aab4ed92c4e5d10139da13
Sam Strasser <sam.strasser@gmail.com>
2014-03-19 12:17:55 -0700

    Update assessment takes

--- info/assessment_attempts.sql
+++ info/assessment_attempts.sql
@@ -1,37 +1,4 @@
-SELECT 
-  count(*)
-  -- courses.name,
-  -- subjects.name,
-  -- know_dos.name,  
-  -- assessment_takes.taken_at, 
-  -- course_know_dos.power, 
-  -- assessment_takes.num_correct, 
-  -- assessment_takes.num_possible,
-  -- know_dos.pcnt_to_pass,
-  -- users.last_leave_on > now() as "Is Currently Enrolled?",
-  -- users.summit_id,
-  -- users.first_name,
-  -- users.last_name
-FROM 
-  course_assignments, 
-  assessment_takes, 
-  course_know_dos, 
-  courses,
-  know_dos, 
-  users,
-  subjects
-WHERE 
-assessment_takes.student_id = users.id and 
-assessment_takes.student_id = course_assignments.student_id AND
-course_assignments.course_id = course_know_dos.course_id AND
-assessment_takes.know_do_id = course_know_dos.know_do_id AND
-course_assignments.course_id = courses.id AND
-course_know_dos.know_do_id=know_dos.id AND
-courses.subject_id = subjects.id and 
-;
-
--- Missing from Bruce's original: 
--- filter out focus areas for courses the student is not enrolled in
+-- TODO: filter out focus areas for courses the student is not enrolled in
 select
   courses.name as "course name",
   subjects.name as "subject name",
@@ -55,14 +22,7 @@ join courses on ckds.course_id = courses.id
 join subjects on courses.subject_id = subjects.id
 join users as students on students.id = ats.student_id
 left outer join sites on students.site_id = sites.id
-where students.summit_id > 9999
-and students.type = 'Student'
+where students.type = 'Student'
+  and sites.name != 'SPS Demo'
 order by students.summit_id, kds.id, ats.taken_at
 ;
-
-
-
-
--- By kid, then assessment, then by date,
--- Add pcnt col
--- Passed
