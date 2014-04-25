ca6a476fbea416a0ad43688de16d0f3fa2a117ea
lizziechoi <lchoi@summitps.org>
2014-04-24 19:56:16 -0700

    Update projects.sql
    Huge favor -
    
    Do you think you could look at this query and see if I correctly added "subject" + "course name"?
    
    And if so, can we update the query, please?
    
    Thanks!
    -lc

--- purple_team/projects.sql
+++ purple_team/projects.sql
@@ -44,11 +44,16 @@ SELECT
   assigners.last_name as assigner_last,
   mentors.first_name as mentor_first,
   mentors.last_name as mentor_last,
-  projects.name as project_name
+  projects.name as project_name,
+  courses.name as course_name,
+  subjects.name as subject_name
 FROM project_assignments as pas
 LEFT OUTER JOIN users as students ON pas.student_id = students.id
 LEFT OUTER JOIN sites ON students.site_id = sites.id
 LEFT OUTER JOIN projects on pas.project_id = projects.id
 LEFT OUTER JOIN users as assigners ON pas.assigned_by_id = assigners.id
 LEFT OUTER JOIN users as mentors ON students.mentor_id = mentors.id
+LEFT OUTER JOIN project_courses as pcs on pas.project_id = pcs.project_id
+LEFT OUTER JOIN courses on pcs.course_id = courses.id
+LEFT OUTER JOIN subjects on subjects.id = courses.subject_id
 ;
